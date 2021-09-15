6c5112570da3f82deb01a2a44c601f0ff3f10327
Leon <hualiang.tian@gmail.com>
2021-09-15 16:20:10 -0400

    commit tree e495a07b18bcc3e54cfca0f569f4cd98f701e3ca

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const driver = { leon: 'silver' , mom: 'black'};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    return Object.assign({}, driver, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
+    driver[key] = value;
+    return driver;
+  }
+
+function deleteFromDriverByKey(driver, key){
+    const newList = Object.assign({}, driver);
+    delete newList[key];
+    return newList;
+}
+
+function destructivelyDeleteFromDriverByKey(drive, key){
+    delete driver[key];
+    return driver;
+}
